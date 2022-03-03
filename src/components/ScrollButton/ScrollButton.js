bc96a840ce6e90abdb3c3b8ea2ae73813e0e35fc
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-03-03 22:16:58 +0100

    added a scrollToTop button

---
+++ src/components/ScrollButton/ScrollButton.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { FaArrowAltCircleUp } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const Button = styled.div`
+    position: fixed;
+    bottom: 30px;
+    right: 40px;
+    font-size: 3rem;
+    z-index: 5;
+    cursor: pointer;
+    color: #26a9df;
+    margin: 0 auto;
+
+    @media ${(props) => props.theme.breakpoints.sm} {
+        right: 15px;
+    }
+`
+
+const ScrollButton = () => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+
+        if(scrolled > 300) {
+            setVisible(true)
+        } else if (scrolled <= 300) {
+            setVisible(false)
+        }
+    }
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    window.addEventListener('scroll', toggleVisible)
+
+  return (
+    <Button>
+        <FaArrowAltCircleUp 
+            onClick={scrollToTop}
+            style={{display: visible ? 'inline' : 'none'}} />
+    </Button>
+  )
+}
+
+export default ScrollButton;
